0c47ff6539d06d4e6091d6310912eb7eaafa2418
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-21 19:40:40 -0300

    Implement function for boxplots

---
+++ plots.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+
+def boxplot(data, labels, title=None, x_label=None, y_label=None):
+    fig, ax = plt.subplots()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    ax.boxplot(data, labels=labels)
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show(bbox_inches='tight')
