3038ddbc5948785befc858cc0d693a8c393ae670
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-21 19:42:51 -0300

    Get PCA components with sklearn

--- e_pca.py
+++ e_pca.py
@@ -1,5 +1,6 @@
 import utils
 import plots
+from sklearn.decomposition import PCA
 
 
 def main():
@@ -7,6 +8,12 @@ def main():
     plots.boxplot(data, labels=variables[1:], title="Not standarized variables", x_label="Variables")
     standarized = utils.standarize_matrix_by_colum(data)
     plots.boxplot(standarized, labels=variables[1:], title="Standarized variables", x_label="Variables")
+    pca = PCA(n_components=2)
+    pca.fit(standarized)
+    print(f"PCA components: {pca.components_}")
+    pcs = pca.transform(standarized)
+    print(f"pc1 y pc2: {pcs}")
+    plots.bargraph(pcs[:, 0], countries, title="", x_label="Countries", y_label="PC1")
 
 
 if __name__ == '__main__':
