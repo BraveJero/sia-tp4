41b193a1c577bffcd16077eb38769251c8473163
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-21 20:32:32 -0300

    Implement biplot

--- e_pca.py
+++ e_pca.py
@@ -5,15 +5,17 @@ from sklearn.decomposition import PCA
 
 def main():
     variables, countries, data = utils.read_data_from_csv("data/europe.csv")
-    plots.boxplot(data, labels=variables[1:], title="Not standarized variables", x_label="Variables")
+    variables = variables[1:]
+    plots.boxplot(data, labels=variables, title="Not standarized variables", x_label="Variables")
     standarized = utils.standarize_matrix_by_colum(data)
-    plots.boxplot(standarized, labels=variables[1:], title="Standarized variables", x_label="Variables")
+    plots.boxplot(standarized, labels=variables, title="Standarized variables", x_label="Variables")
     pca = PCA(n_components=2)
     pca.fit(standarized)
     print(f"PCA components: {pca.components_}")
     pcs = pca.transform(standarized)
     print(f"pc1 y pc2: {pcs}")
     plots.bargraph(pcs[:, 0], countries, title="", x_label="Countries", y_label="PC1")
+    plots.biplot(pcs[:, 0], pcs[:, 1], countries, variables, pca.components_)
 
 
 if __name__ == '__main__':
--- plots.py
+++ plots.py
@@ -6,10 +6,10 @@ def boxplot(data, labels, title=None, x_label=None, y_label=None):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    ax.boxplot(data, labels=labels)
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
-    plt.show(bbox_inches='tight')
+    ax.boxplot(data, labels=labels)
+    plt.show()
 
 
 def bargraph(data, labels, title=None, x_label=None, y_label=None):
@@ -17,7 +17,39 @@ def bargraph(data, labels, title=None, x_label=None, y_label=None):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    ax.bar(labels, data)
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
-    plt.show(bbox_inches='tight')
+    ax.bar(labels, data)
+    plt.show()
+
+
+def biplot(pc1, pc2, labels, features, loadings, title=None, x_label=None, y_label=None):
+    fig, ax = plt.subplots()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    scale_pc1 = 1.0 / (pc1.max() - pc1.min())
+    scale_pc2 = 1.0 / (pc2.max() - pc2.min())
+
+    for i, feature in enumerate(features):
+        ax.arrow(0, 0, loadings[0, i],
+                 loadings[1, i])
+        ax.text(loadings[0, i] * 1.15,
+                loadings[1, i] * 1.15,
+                feature, fontsize=10)
+
+    ax.scatter(pc1 * scale_pc1, pc2 * scale_pc2)
+    for (i, label) in enumerate(labels):
+        ax.annotate(label,
+                    (pc1[i] * scale_pc1, pc2[i] * scale_pc2),
+                    fontsize=10)
+
+    ax.set_xlabel('PC1', fontsize=20)
+    ax.set_ylabel('PC2', fontsize=20)
+    ax.set_title('Figure 1', fontsize=20)
+    plt.figure()
+
+    plt.show()
