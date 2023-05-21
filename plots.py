edc3326f756f106026b38d45830268e1fb4528ea
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-21 19:41:02 -0300

    Implement function for bargraphs

--- plots.py
+++ plots.py
@@ -10,3 +10,14 @@ def boxplot(data, labels, title=None, x_label=None, y_label=None):
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.show(bbox_inches='tight')
+
+
+def bargraph(data, labels, title=None, x_label=None, y_label=None):
+    fig, ax = plt.subplots()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    ax.bar(labels, data)
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show(bbox_inches='tight')
