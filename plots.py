6aba2bf220c341830e0c2db9f85566eea033bd18
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-22 23:10:14 -0300

    Remove diagonal orientation

--- plots.py
+++ plots.py
@@ -17,9 +17,8 @@ def bargraph(data, labels, title=None, x_label=None, y_label=None):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.xticks(rotation=45, ha='right')
-    plt.tight_layout()
-    ax.bar(labels, data)
+    plt.subplots_adjust(left=0.25)
+    ax.barh(labels, data)
     plt.show()
 
 
