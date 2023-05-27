6b814002358df80f8f5bd5b467eb1eddbd2238bb
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-05-27 14:01:15 -0300

    add all letters and find most orthogonal ones

--- hopfield_main.py
+++ hopfield_main.py
@@ -1,33 +1,9 @@
-import numpy as np
-
 import plots
 import utils
 from src.hopfield.hopfield import HopfieldNetwork
+from src.hopfield.letters import *
 
 # Define the alphabet matrices
-J = [1, 1, 1, 1, 1,
-     -1, -1, 1, -1, -1,
-     -1, -1, 1, -1, -1,
-     1, -1, 1, -1, -1,
-     1, 1, 1, -1, -1]
-
-A = [-1, 1, 1, 1, -1,
-     1, -1, -1, -1, 1,
-     1, 1, 1, 1, 1,
-     1, -1, -1, -1, 1,
-     1, -1, -1, -1, 1]
-
-Z = [1, 1, 1, 1, 1,
-     -1, -1, -1, 1, -1,
-     -1, -1, 1, -1, -1,
-     -1, 1, -1, -1, -1,
-     1, 1, 1, 1, 1]
-
-B = [1, 1, 1, 1, -1,
-     1, -1, -1, -1, 1,
-     1, 1, 1, 1, -1,
-     1, -1, -1, -1, 1,
-     1, 1, 1, 1, -1]
 
 espureo = [-1, 1, 1, 1, -1,
            1, -1, -1, -1, 1,
@@ -37,6 +13,7 @@ espureo = [-1, 1, 1, 1, -1,
 
 # Combine the matrices into a single vector
 letters = [J, A, Z, B]
+orthogonal = [P, M, L, I]
 
 
 def main():
