569ebc83986c8de6cdc2f7cd4e56a64622c19c65
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-05-29 18:29:56 -0300

    calculate all permutations orthogonality

--- src/hopfield/letters.py
+++ src/hopfield/letters.py
@@ -1,3 +1,7 @@
+import itertools
+
+import numpy as np
+
 A = [-1, 1, 1, 1, -1,
      1, -1, -1, -1, 1,
      1, 1, 1, 1, 1,
@@ -153,3 +157,22 @@ Z = [1, 1, 1, 1, 1,
      -1, -1, 1, -1, -1,
      -1, 1, -1, -1, -1,
      1, 1, 1, 1, 1]
+
+l = {"A": A, "B": B, "C": C, "D": D, "E": E, "F": F, "G": G, "H": H, "I": I, "J": J, "K": K, "L": L, "M": M, "N": N,
+     "O": O, "P": P, "Q": Q, "R": R, "S": S, "T": T, "U": U, "V": V, "W": W, "X": X, "Y": Y, "Z": Z}
+
+
+def get_letter_combinations_orthogonality():
+    results = []
+    for letters in itertools.permutations(l.keys(), 4):
+        if letters not in results:
+            results.append(letters)
+
+            sum = 0
+
+            for i in range(len(letters)):
+                for j in range(i + 1, len(letters)):
+                    dot = np.dot(l[letters[i]], l[letters[j]])
+                    sum += np.abs(dot)
+            if sum < 12:
+                print(letters, sum)
